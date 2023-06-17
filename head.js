3e5c8bd3b3e032597415720942dceb9c39d05aaa
Shanel Beebe <shanelbeebe@icloud.com>
2023-06-17 16:41:23 +0000

    added head.js file and function

---
+++ head.js
@@ -0,0 +1,16 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅ 🥳 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 😥 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = arr => {
+  return arr[0]
+}
+
+console.log(head([5,6,7], 5))
+console.log(head(['hello', 'world', '!']))
+console.log(head([]))
+console.log(head(['Shanel']))
