cfe962cdf795c44954c5e24a458265443bfb918e
Shanel Beebe <shanelbeebe@icloud.com>
2023-06-17 19:02:40 +0000

    added tail.js

---
+++ tail.js
@@ -0,0 +1,19 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ 🥳 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 😥 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = (arr) => {
+  const arrTail = arr.slice(1)
+  return arrTail
+}
+
+console.log(tail([1, 2, 3, 4]))
+
+const nums = [1,3,5,7]
+const result = tail(nums)
+
+assertEqual(result, tail(nums))
