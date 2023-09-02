043a471dc9d4275e0e8701f0263d2501a9fd6639
Shanel Beebe <shanelbeebe@icloud.com>
2023-09-02 02:48:03 +0000

    more work on flatten.js

--- flatten.js
+++ flatten.js
@@ -24,25 +24,19 @@ const assertArraysEqual = (arr1, arr2) => {
 
 const flatten = (arr) => {
   const flatArray = []
-  for (let i = 0; i < arr.length; i++){
+  for (let i = 0; i < arr.length; i++) {
     if (!Array.isArray(arr[i])) {
       flatArray.push(arr[i]);
     } else {
-      for (let j = 0; j < arr[i].length; j++){
-        if (Array.isArray(arr[i][j])) {
-          let nested = (arr[i][j])
-          flatten(nested)
-        } else {
+      for (let j = 0; j < arr[i].length; j++) {
+       
           flatArray.push(arr[i][j]);
         }
       }
-      }
+    }
+    return flatArray;
   }
-  return flatArray;
-}
 
 
-
-console.log(flatten([1, 2, [3, 4], 5, [6, [7, 8, 9, [10, 11, [12, 13, 14]]]]]));
 console.log(flatten([1, 2, [3, 4], 5, [6], [7, 8, 9, 10], 11, 12, 13, [14]]));
 console.log(flatten([1, 2, [3, 4], 5, [6]]));
