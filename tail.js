5bb75fb4639b6e1a59e6f73250def39024f76987
Shanel Beebe <shanelbeebe@icloud.com>
2024-02-02 20:37:03 +0000

    made updates to tail.js

--- tail.js
+++ tail.js
@@ -17,12 +17,9 @@ const resultZero = tail([]);
 assertEqual(result.length, 2);
 assertEqual(result[0], "Lighthouse");
 assertEqual(result[1], "Labs");
-assertEqual(resultOne[0], undefined);
-assertEqual(resultZero[0], undefined);
+assertEqual(resultOne.length, 0);
+assertEqual(resultZero.length, 0);
 
 const words = ["Yo", "Yo", "Ma"];
 tail(words);
 assertEqual(words.length, 3);
-
-
-
