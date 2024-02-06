371617ad4867e4e653c465cc62523f293456bb3c
Shanel Beebe <shanelbeebe@icloud.com>
2024-02-06 21:42:44 +0000

    updated findKey.js

--- findKey.js
+++ findKey.js
@@ -9,26 +9,25 @@ const assertEqual = function (actual, expected) {
 const findKey = (object, callback) => {
   for (let key in object) {
     if (callback(object[key])) {
-      return key
+      return key;
     }
   }
-  return
-}
+};
 
 assertEqual(findKey({
   "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), "noma")
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2), "noma");
 
 assertEqual(findKey({
   "LOTR": { books: 3 },
-  "Harry Potter":   { books: 7 },
-  "The Hunger Games":      { books: 4 },
-  "Twilight":   { books: 4 },
-  "Chronicles of Narnia":       { books: 7 },
-  "The Mortal Instruments":  { books: 6 }
-}, x => x.books === 4), "The Hunger Games")
+  "Harry Potter": { books: 7 },
+  "The Hunger Games": { books: 4 },
+  "Twilight": { books: 4 },
+  "Chronicles of Narnia": { books: 7 },
+  "The Mortal Instruments": { books: 6 }
+}, x => x.books === 4), "The Hunger Games");
