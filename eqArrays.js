e39ee32f19d61735a495a14b5aa599abc989172c
Shanel Beebe <shanelbeebe@icloud.com>
2024-02-06 21:26:32 +0000

    updated eqArrays.js to be concise and implemented map.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,4 +1,4 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅ 🥳 Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -7,29 +7,22 @@ const assertEqual = function (actual, expected) {
 };
 
 const eqArrays = (arr1, arr2) => {
-  // empty array variable to put true/false values
-  let isEqual = [];
   // Check if length of arrays match
   if (arr1.length === arr2.length) {
     // if length matches, loop through each item in array 1
-    arr1.forEach((item, i) => {
+    for (let i = 0; i < arr1.length; i++) {
       // compare each item in both arrays
-      if (item === arr2[i]) {
+      if (arr1[i] !== arr2[i]) {
         // push true value to isEqual array
-        isEqual.push(true)
-      } else {
-        // push false value to isEqual array
-        isEqual.push(false);
+        return false;
       }
-    })
-  } else {
-    // if array lengths don't match push false value to isEqual array
-    isEqual.push(false);
+    }
+    return true;
   }
-  return  isEqual.includes(false) ? false : true
-}
+  return false;
+};
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // --> Pass: Arrays match length and values 
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // --> Fail: Arrays match length but not values
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // --> Pass: Arrays match length and values
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // --> Fail: Arrays match length but not values
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", "4"]), true); // --> Fail: Arrays do not match length
 assertEqual(eqArrays(["1", "2", "3"], ["1", 2, "3"]), true); // --> Fail: Array values do not strictly equal
--- map.js
+++ map.js
@@ -1,17 +1,17 @@
 const eqArrays = (arr1, arr2) => {
-  let isEqual = [];
+  // Check if length of arrays match
   if (arr1.length === arr2.length) {
-    arr1.forEach((item, i) => {
-      if (item === arr2[i]) {
-        isEqual.push(true);
-      } else {
-        isEqual.push(false);
+    // if length matches, loop through each item in array 1
+    for (let i = 0; i < arr1.length; i++) {
+      // compare each item in both arrays
+      if (arr1[i] !== arr2[i]) {
+        // push true value to isEqual array
+        return false;
       }
-    });
-  } else {
-    isEqual.push(false);
+    }
+    return true;
   }
-  return isEqual.includes(false) ? false : true;
+  return false;
 };
 
 const assertArraysEqual = (arr1, arr2) => {
@@ -24,7 +24,7 @@ const assertArraysEqual = (arr1, arr2) => {
 
 const words = ["ground", "control", "to", "major", "tom"];
 
-const map = function (array, callback) {
+const map = function(array, callback) {
   const results = [];
   for (let item of array) {
     results.push(callback(item));
@@ -36,10 +36,10 @@ const results1 = map(words, (word) => word[0]);
 assertArraysEqual(results1, ["g", "c", "t", "m", "t"]);
 
 const results2 = map(words, (word) => {
-  const wordArray = word.split('');
-  const reverseArray = wordArray.reverse()
-  const joinArray = reverseArray.join('')
-  return joinArray
+  const wordArray = word.split("");
+  const reverseArray = wordArray.reverse();
+  const joinArray = reverseArray.join("");
+  return joinArray;
 });
 assertArraysEqual(results2, ["dnuorg", "lortnoc", "ot", "rojam", "mot"]);
 
