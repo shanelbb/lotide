db4b6529b4c2239cf08c331ed3d46b92dc2af000
Shanel Beebe <shanelbeebe@icloud.com>
2024-02-18 02:27:41 +0000

    installed mocha/chai and implemented chai into headTest.js

--- middle.js
+++ middle.js
@@ -1,12 +1,18 @@
+// Function that returns an new array with the middle value(s) in an array
 const middle = (arr = []) => {
+  // empty array to hold the middle value(s)
   const midValue = [];
+  // variable that finds the middle index of the array
   let midIndex = Math.floor(arr.length / 2);
+  // if array length is less than 3, return an empty array
   if (arr.length < 3) {
     return [];
   } else if (arr.length % 2 !== 0) {
+    // if array length is odd add value at midIndex to the midValue array and return midValue
     midValue.push(arr[midIndex]);
     return midValue;
   } else {
+    // if array length is even, add values just before and at midIndex to midValue array and return midValue
     midValue.push(arr[midIndex - 1], arr[midIndex]);
     return midValue;
   }
--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,20 @@
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
 const head = require("../head");
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["hello", "world", "!"]), "hello");
-assertEqual(head([]), undefined);
-assertEqual(head(["Shanel"]), "Shanel");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(["5"]), "5");
+  });
+  it("returns 'hello' for ['hello', 'world', '!']", () => {
+    assert.strictEqual(head(["hello", "world", "!"]), "hello");
+  });
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+  it("returns 'Goodbye' for ['Goodbye']", () => {
+    assert.strictEqual(head(["Goodbye"]), "Goodbye");
+  });
+});
