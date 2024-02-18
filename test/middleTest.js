08155c0ca426960251dddcb5c1c88381905e2f87
Shanel Beebe <shanelbeebe@icloud.com>
2024-02-18 03:12:13 +0000

    added index.js file to containt the lotide object

--- test/middleTest.js
+++ test/middleTest.js
@@ -26,4 +26,10 @@ describe("#middle", () => {
   it("returns [8, 15] for [1, 4, 3, 8, 15, 7, 9, 14]", () => {
     assert.deepEqual(middle([1, 4, 3, 8, 15, 7, 9, 14]), [8, 15]);
   });
+  it("makes sure the original array was not altered by the middle function", () => {
+    const words = ["hello", "world", "lighthouse"];
+    const result = middle(words);
+    assert.deepEqual(result, ["world"]);
+    assert.strictEqual(words.length, 3);
+  });
 });
