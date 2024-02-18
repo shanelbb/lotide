820fed4cd107ea64eb5a8ecab3a17de356e80ec5
Shanel Beebe <shanelbeebe@icloud.com>
2024-02-18 01:39:53 +0000

    implemented eqArraysTest file

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ 🥳 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 😥 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = (arr1, arr2) => {
   // Check if length of arrays match
   if (arr1.length === arr2.length) {
@@ -22,7 +14,4 @@ const eqArrays = (arr1, arr2) => {
   return false;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // --> Pass: Arrays match length and values
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // --> Fail: Arrays match length but not values
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", "4"]), true); // --> Fail: Arrays do not match length
-assertEqual(eqArrays(["1", "2", "3"], ["1", 2, "3"]), true); // --> Fail: Array values do not strictly equal
+module.exports = eqArrays;
